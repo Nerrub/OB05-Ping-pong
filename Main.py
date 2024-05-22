d8ad637bd7f0d04c9ebba4bbaf837cde9c172bd8
Nerrub <Lyapin_dmitriy@hotmail.com>
2024-05-22 10:59:01 +0300

    3

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Ping Pong')
+pygame.display.set_caption('Пин-понг)
 
 # Цвета
 black = (0, 0, 0)
